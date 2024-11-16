67d744dc03d223bc53cd8cfa8ec56a8e120dbdc8
JH <jiherngl@gmail.com>
2024-11-16 15:47:51 +0800

    ST2195_assignment_04

--- foo.R
+++ foo.R
@@ -7,6 +7,7 @@ getwd()
 r<- 2
 pi<- 3.14
 
+
 #Function to compute the volume of a sphere with radius r
 volume<- function(r){
   VLsphere<- 3/4 *pi *r^2
@@ -15,15 +16,13 @@ volume<- function(r){
 volume(2)
 
 #Function to compute the volumes of the spheres with radius r, r^2 and r^3
-volume_vector <- (c(r,r^2,r^3))
 volume_vector <- function(r) {
-  for (r in 1:22){
+  for (r in 22) {
     volume(r)
     volume(r^2)
     volume(r^3)
   }
 }
-
 #Run volume_vector(r) and print the volumes of the spheres with r, r^2 and r^3
 volume_vector(r)
 
